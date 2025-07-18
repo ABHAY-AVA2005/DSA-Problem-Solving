c57a2b03915d37db9174af1893b86387547bba38
Abhay Varshit  Aripirala <abhayvarshit2005@gmail.com>
2025-07-18 21:23:25 +0530

    [283] [Time Beats: 100.00%] [Memory Beats: 52.64%] - LeetPush

---
+++ Move-Zeroes.cpp
@@ -0,0 +1,23 @@
+/// using 2 ptrs
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[count]);
+                count++;
+            }
+        }
+
+            while(count<n)
+            {
+                nums[count++]=0;
+            }
+            
+        }
+};
